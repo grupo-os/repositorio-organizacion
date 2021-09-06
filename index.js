3ee8fe05ba8fb540287013ec9410d73848b302e8
santiR22 <santiagorodas688@gmail.com>
2021-09-06 15:19:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/home', (req, res) => res.send('Servidor Express funcionando'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
