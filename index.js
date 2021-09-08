a25291843fd8f98938f8d47da4d26c3612bea915
gonnzalezemma <jorge_99.l@hotmail.com>
2021-09-08 10:12:08 -0300

    Agregue carpetas y mongo

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/home', (req, res) => res.send('Servidor Express funcionando'))
+app.get('/home', (req, res) => res.send(console.log('Servidor Express funcionando')))
 app.listen(port, () => console.log(`servidor corriendo en el puerto 4000`))
-
