2067c67fe70bd795ee9fa879fb93059e57f26f46
santiR22 <santiagorodas688@gmail.com>
2021-09-16 19:35:51 -0300

    agregado archivo .env

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const { urlencoded } = require('express');
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = 4000
+
+//Middlewares...
+app.use(express.json());
+app.use(urlencoded({extended: false}))
+
+//Settings...
+app.set('port', process.env.PORT || 4000)
 
 app.get('/home', (req, res) => res.send(console.log('Servidor Express funcionando')))
 app.listen(port, () => console.log(`servidor corriendo en el puerto 4000`))
