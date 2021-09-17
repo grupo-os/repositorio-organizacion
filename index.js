68c4a541787ddb5e4e19b7f70c56864494073b51
santiR22 <santiagorodas688@gmail.com>
2021-09-16 21:15:41 -0300

    controladores y rutas

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { urlencoded } = require('express');
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const port = 4000
+const express = require('express');
+require('dotenv').config();
+const app = express();
 
 //Middlewares...
 app.use(express.json());
-app.use(urlencoded({extended: false}))
+app.use(urlencoded({extended: false}));
 
 //Settings...
-app.set('port', process.env.PORT || 4000)
+app.set('port', process.env.PORT || 4000);
+
+//Routes...
+app.use(require('./routes/user.routes'));
 
 app.get('/home', (req, res) => res.send(console.log('Servidor Express funcionando')))
 app.listen(port, () => console.log(`servidor corriendo en el puerto 4000`))
--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
 const {model, Schema} = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = model('User', UserSchema)
