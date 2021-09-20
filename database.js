5476ded946ff60e9801569b7d99949ebed6c3374
santiR22 <santiagorodas688@gmail.com>
2021-09-20 16:50:33 -0300

    .

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const conectarDB = () => {
-    mongoose.connect('mongodb://localhost:27017/serverdb')
+    mongoose.connect(process.env.URI)
     .then(() => console.log('Conexion a BD exitosa!!!'))
     .catch((error) => console.log('Fallo la conexion a la BD: ', error))
 }
